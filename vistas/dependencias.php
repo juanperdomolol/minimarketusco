00558c4f3585330a453d97e3943ad45a7e62696e
juanperdomolol <juanpb902@gmail.com>
2020-08-19 20:51:29 -0500

    cambios

--- vistas/dependencias.php
+++ vistas/dependencias.php
@@ -2,8 +2,23 @@
 <link rel="stylesheet" type ="text/css" href="../librerias/alertifyjs/css/themes/default.css">
 <link rel="stylesheet" type ="text/css" href="../librerias/bootstrap-4.5.2-dist/css/bootstrap.css">
 <link rel="stylesheet" type ="text/css" href="../librerias/select2-4.0.3/dist/css/select2.css">
+<link rel="stylesheet" type ="text/css" href="../css/menu.css">
 
 <script src="../librerias/jquery-3.5.1.min.js"></script>
 <script src="../librerias/alertifyjs/alertify.js"></script>
 <script src="../librerias/bootstrap-4.5.2-dist/js/bootstrap.js"></script>
 <script src="../librerias/select2-4.0.3/dist/js/select2.js"></script>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<link rel="stylesheet" type="text/css" href="../librerias/alertifyjs/css/alertify.css">
+<link rel="stylesheet" type="text/css" href="../librerias/alertifyjs/css/themes/default.css">
+<link rel="stylesheet" type="text/css" href="../librerias/bootstrap/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="../librerias/select2/css/select2.css">
+<link rel="stylesheet" type="text/css" href="../css/menu.css">
+
+<script src="../librerias/jquery-3.2.1.min.js"></script>
+<script src="../librerias/alertifyjs/alertify.js"></script>
+<script src="../librerias/bootstrap/js/bootstrap.js"></script>
+<script src="../librerias/select2/js/select2.js"></script>
+<script src="../js/funciones.js"></script>
