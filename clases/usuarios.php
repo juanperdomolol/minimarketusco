0026c695a6181bba0c055d4c761d416ba9641522
juanperdomolol <juanpb902@gmail.com>
2020-08-19 19:10:55 -0500

    programacion de login, creando la sesion

--- clases/usuarios.php
+++ clases/usuarios.php
@@ -1,11 +1,13 @@
-<?php  
-    class usuarios{
-        public function registroUsuario($datos){
-            $c=new conectar();
-            $conexion=$c->conexion();
+<?php
+class usuarios
+{
+    public function registroUsuario($datos)
+    {
+        $c = new conectar();
+        $conexion = $c->conexion();
 
-            $fecha = date('Y-m-d');
-            $sql="INSERT into usuarios
+        $fecha = date('Y-m-d');
+        $sql = "INSERT into usuarios
                             (nombre,
                             apellido,
                             email,
@@ -16,8 +18,39 @@
                             '$datos[2]', 
                             '$datos[3]', 
                             '$fecha')";
-         return mysqli_query($conexion,$sql);                   
+        return mysqli_query($conexion, $sql);
+    }
+    public function loginUser($datos)
+    {
+        $c = new conectar();
+        $conexion = $c->conexion();
+        $password = sha1($datos[1]);
 
+        $_SESSION['usuario'] = $datos[0];
+        $_SESSION ['iduser']= self::traeId($datos);
+        
+        $sql = "SELECT * 
+                    from usuarios
+                    where email='$datos[0]' 
+                    and password='$password'";
+        $result = mysqli_query($conexion, $sql);
+        if (mysqli_num_rows($result) > 0) {
+            return 1;
+        } else {
+            return 0;
         }
     }
-?>
+    public function traeId($datos)
+    {
+        $c = new conectar();
+        $conexion = $c->conexion();
+        $password=sha1($datos[1]);
+
+        $sql="SELECT id_usuario 
+                from usuarios
+                    where email='$datos[0]' 
+                    and password='$password'";
+    $result=mysqli_query($conexion,$sql);
+    return mysqli_fetch_row($result)[0];            
+    }
+}
--- index.php
+++ index.php
@@ -1,28 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>Login Usuario</title>
     <link rel="stylesheet" type="text/css" href="librerias/bootstrap-4.5.2-dist/css/bootstrap.css">
     <script src="librerias/jquery-3.5.1.min.js"></script>
+    <script src="js/funciones.js"></script>s
 </head>
+
 <body style="background-color: gray;">
     <br><br><br>
     <div class="container justify-content-center">
         <div class="row justify-content-center">
             <div class="col-sm-6">
-                <div class="card shadow p-3 mb-5 bg-white rounded text-white" >
+                <div class="card shadow p-3 mb-5 bg-white rounded text-white">
                     <div class="card-header bg-dark text-center">Sistema de ventas</div>
                     <div class="card-body bg-white text-dark"><img class="mx-auto d-block" src="img/Usco.png" height="170px">
-                        <form>
+                        <form id="frmLogin">
                             <div class="form-group ">
                                 <label>Usuario</label>
                                 <input type="text" class="form-control" name="usuario" id="usuario">
                             </div>
                             <div class="form-group ">
                                 <label>Contraseña</label>
-                                <input type="text" class="form-control" name="contraseña" id="contraseña">
+                                <input type="text" class="form-control" name="password" id="password">
                             </div>
-                            <button class="btn btn-primary">Entrar</button>
+                            <button class="btn btn-primary" id="EntrarSistema">Entrar</button>
                             <a href="registro.php" class="btn btn-danger">Registrarse</a>
                         </form>
                     </div>
@@ -30,6 +33,34 @@
             </div>
         </div>
     </div>
-    
+
 </body>
+
 </html>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#EntrarSistema').click(function() {
+
+            vacios = validarFormVacio('frmLogin');
+            if (vacios > 0){
+                alert("Debes llenar todos los campos!!");
+                return false;
+            }
+            datos = $('#frmLogin').serialize();
+            $.ajax({
+                type: "POST",
+                data: datos,
+                url: "procesos/regLogin/login.php",
+                success: function(r) {
+                    if(r==1){
+                        window.location= "vistas/inicio.php";
+                    }else{
+                        alert("No se pudo acceder :( ");
+                    }
+
+                }
+            });
+        });
+    })
+</script>
